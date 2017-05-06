1fd6e5f38c18cd10a7dbc8b047737a33ca3dc23b
Bruno Filgueiras <fbfdsouza@gmail.com>
2017-05-06 17:02:11 -0300

    dummy login page, users table sessions and securepassword

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
