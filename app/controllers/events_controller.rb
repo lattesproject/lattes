f973a49d5bd69f9160be951c92cd782f2456c337
Bruno Filgueiras <fbfdsouza@gmail.com>
2017-05-07 16:55:59 -0300

    add and destroy events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,5 +4,35 @@ class EventsController < ApplicationController
 		@events = Event.paginate(page: params[:page], per_page: 5)
 	end
 
+	def new
+		@event = Event.new
+	end
 
+	def create
+		@event = Event.new(event_params)
+		@event.user = current_user
+		if @event.save
+			flash[:success] = "Article was successfully created"
+			redirect_to event_path(@event)
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		@event = Event.find(params[:id])
+	end
+
+	def destroy
+		@event = Event.find(params[:id])
+		@event.destroy
+		flash[:danger] = "Article was successfully deleted"
+		redirect_to events_path
+	end
+
+	def event_params
+		params.require(:event).permit(:event_name, :article_qualis_a1, :article_qualis_a2, :article_qualis_b1,
+			:article_qualis_b2, :article_qualis_b3, :article_qualis_b4, :article_qualis_b5, :article_qualis_c)
+	end
+	
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ActiveRecord::Base
 	belongs_to :user
 	validates :user_id, presence: true
-	validates :event_name, presence: true
+	validates :event_name, presence: true,
+	uniqueness: { case_sensitive: false },
+	length: { minimum: 3, maximum: 25 }
 end
