9f04917ba156628da31be48a32a6535cb4c1172a
Bruno Filgueiras <fbfdsouza@gmail.com>
2017-05-07 15:04:35 -0300

    adding footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'about' , to: 'pages#about'
   resources :users, except: [:new]
   resources :events, except: [:edit]
   get 'signup', to: 'users#new'
