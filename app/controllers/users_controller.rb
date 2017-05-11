2afb21404be657b62b06ec0bb1542d0a76ad46f5
franciscobrunofilgueiras <fbfdsouza@gmail.com>
2017-05-11 08:53:13 -0300

    listing users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
 	def destroy
 		@user = User.find(params[:id])
 		@user.destroy
-		flash[:danger] = "User and all articles created by user have been deleted"
+		flash[:danger] = "User and all articles events by user have been deleted"
 		redirect_to users_path
 	end
 
