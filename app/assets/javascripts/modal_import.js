a2d59c5abad1ac2a539579e7a44a9fe0ba57586e
Alex Nogueira <alexnogueira.ti@gmail.com>
2017-05-22 22:26:07 -0300

    Fix modal import cv

--- app/assets/javascripts/modal_import.js
+++ app/assets/javascripts/modal_import.js
@@ -1,5 +1,15 @@
-$(function () {
-    $(".custom-close").on('click', function() {
-        $('#modal-import-cv').modal('hide');
+$(document).ready(function() {
+    $('#modal-import-cv').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var event = button.data('event') // Extract info from data-* attributes
+        
+        // Update the- modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var modal = $(this)
+        modal.find('#events_event_id').val(event)
     });
+
+    $('#modal-import-cv').on('hidden.bs.modal', function (event) {
+        $(this).find("input,textarea,select").val('').end();
+    });
+
 });
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.assets.precompile += %w( main.js )
+  config.assets.precompile += %w( modal_import.js )
+  
 end
