ff109e5d8a72243a20c61f06412fe32abe2a9e61
Bruno Filgueiras <fbfdsouza@gmail.com>
2017-06-18 18:57:21 -0300

    fixing adding candidates

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,16 +1,5 @@
 class Candidate < ActiveRecord::Base
-	before_save :default_values
 	validates :event_id, presence: true
 	belongs_to :event
 
-
-
-
-private
-	def default_values
-    self.livros ||= 0 # note self.status = 'P' if self.status.nil? might be safer (per @frontendbeauty)
-  end
 end
-
-
-
